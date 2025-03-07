84b7f254ef5db055f81504b1f63cb8b859a39a92
Chandan Kahar <chandankahar2010@gmail.com>
2025-03-07 12:37:19 +0530

    sample_env added with secret key in env file

--- app/config.py
+++ app/config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(env_path) 
 class Config:
     """Configuration settings loaded from .env"""
     DB_HOST = os.getenv("DB_HOST")
@@ -10,3 +10,5 @@ class Config:
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ALGORITHM = os.getenv("ALGORITHM")
--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,6 @@
 from passlib.context import CryptContext
 from jwt import encode
-
-SECRET_KEY = "8812e70f4856b21acbdef0d34f44ff0465eb4fc989fe5b723e28b5e34648e528"
-ALGORITHM = "HS256"
+from .config import Config
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -13,4 +11,4 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_token(data: dict):
-    return encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    return encode(data, Config.SECRET_KEY, algorithm=Config.ALGORITHM)
